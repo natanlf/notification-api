bfae3b8fcc3d67407de38efcfa4833a8b42f77c7
Natan <natanlaraferreira@gmail.com>
2025-01-16 22:31:45 -0300

    first commit

---
+++ src/test/java/com/natancode/notification/NotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natancode.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
